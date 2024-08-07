344066c6cbadf4f56430e82719075a9cde349d5e
jit02git <jit02sep@gmail.com>
2024-08-08 00:43:42 +0530

    core concept of React

---
+++ App.js
@@ -0,0 +1,13 @@
+const heading = React.createElement(
+    "h1",
+    {
+      id: "heading",
+      xyz:"abc"
+    },  
+    "Hello World from React JS"
+
+);
+console.log(heading);
+
+const root =  ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
